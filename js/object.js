4d49fb1aeed480018dd3a74d5d2931cf0ebb8511
Mlaurgaard <Mlaurgaard@users.noreply.github.com>
2018-04-09 11:10:16 +0200

    Added imgsilder.css. buildt an image slider into object.js.

--- js/object.js
+++ js/object.js
@@ -1,1 +1,16 @@
 /*objects*/
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var 1;
+    var x = document.getElementsByClassName("")
+    for (i = 0; 1 < x.lenght; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.lenght) {slideIndex = 1}
+    x[slideindex-1].style.display ="block";
+    setTimeout(carousel, 7000); 
+}
